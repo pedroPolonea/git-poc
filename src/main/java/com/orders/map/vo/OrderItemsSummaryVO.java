75ee8484169c6e881d5f1aa57658e7112844a7e3
pedroPolonea <phpolonea@gmail.com>
2019-06-15 18:45:51 -0300

    XPTO-01-Incluindo id's.

--- src/main/java/com/orders/map/vo/OrderItemsSummaryVO.java
+++ src/main/java/com/orders/map/vo/OrderItemsSummaryVO.java
@@ -1,5 +1,17 @@
 package com.orders.map.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItemsSummaryVO {
 
+    private Long id;
+
+    private Long idOrder;
 }
